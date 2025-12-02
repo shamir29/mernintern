a739f6aa8327c3d1f0c8024886476885ac0f45fe
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-02 10:39:12 +0530

    Styled components

--- src/hello-world/hello-world-component.jsx
+++ src/hello-world/hello-world-component.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { HelloWorldStyle } from "./hello-world-style";
 const HelloWorldComponent = () => {
-  return <h1>Sri eshwar</h1>;
+  return (
+    <HelloWorldStyle>
+      <h1 className="element">Sri eshwar</h1>
+    </HelloWorldStyle>
+  );
 };
 export default HelloWorldComponent;
---
+++ src/hello-world/hello-world-style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const HelloWorldStyle = styled.div`
+  background-color: green;
+  .element {
+    color: red;
+  }
+`;
